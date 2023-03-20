cc0c92d6ad041d67f8c7575e1e658a3b07815561
Noah Zinsmeister <noahwz@gmail.com>
2023-03-20 14:56:16 -0400

    add files from core

---
+++ contracts/hooks/VolatilityOracle.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.13;
+
+import {IPoolManager} from "@uniswap/core-next/contracts/interfaces/IPoolManager.sol";
+import {IDynamicFeeManager} from "@uniswap/core-next/contracts/interfaces/IDynamicFeeManager.sol";
+import {BaseHook} from "./BaseHook.sol";
+
+contract VolatilityOracle is BaseHook, IDynamicFeeManager {
+    uint32 deployTimestamp;
+
+    function getFee(IPoolManager.PoolKey calldata) external view returns (uint24) {
+        uint24 startingFee = 3000;
+        uint32 lapsed = _blockTimestamp() - deployTimestamp;
+        return startingFee + (uint24(lapsed) * 100) / 60; // 100 bps a minute
+    }
+
+    /// @dev For mocking
+    function _blockTimestamp() internal view virtual returns (uint32) {
+        return uint32(block.timestamp);
+    }
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
+        Hooks.validateHookAddress(
+            this,
+            Hooks.Calls({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeModifyPosition: false,
+                afterModifyPosition: false,
+                beforeSwap: false,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false
+            })
+        );
+        deployTimestamp = _blockTimestamp();
+    }
+}
