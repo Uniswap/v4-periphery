bea74c7e471afec0b15a4b86392c331a0aea3ae5
Allen Lin <allenlin1992@hotmail.com>
2023-07-10 12:40:07 -0400

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.8.0;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./base/PeripheryImmutableState.sol";
+import "./base/PeripheryValidation.sol";
 import "./interfaces/INonfungiblePositionManager.sol";
 
-contract NonfungiblePositionManager is ERC721, PeripheryImmutableState, INonfungiblePositionManager {
+contract NonfungiblePositionManager is
+    ERC721,
+    PeripheryImmutableState,
+    INonfungiblePositionManager,
+    PeripheryValidation
+{
     constructor(address _poolManager, address _WETH9)
         ERC721("Uniswap V4 Positions NFT-V1", "UNI-V4-POS")
         PeripheryImmutableState(_poolManager, _WETH9)
     {}
 
-    function mint() external {}
+    function mint(MintParams calldata params)
+        external
+        payable
+        override
+        checkDeadline(params.deadline)
+        returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1)
+    {}
 }
--- contracts/interfaces/INonfungiblePositionManager.sol
+++ contracts/interfaces/INonfungiblePositionManager.sol
@@ -2,6 +2,29 @@
 pragma solidity ^0.8.0;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import './IPeripheryImmutableState.sol';
+import "./IPeripheryImmutableState.sol";
 
-interface INonfungiblePositionManager is IERC721, IPeripheryImmutableState {}
+interface INonfungiblePositionManager is IERC721, IPeripheryImmutableState {
+    struct MintParams {
+        bytes32 poolKey;
+        int24 tickLower;
+        int24 tickUpper;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        address recipient;
+        uint256 deadline;
+    }
+
+    /// @notice Creates a new position wrapped in a NFT
+    /// @param params The params necessary to mint a position, encoded as `MintParams` in calldata
+    /// @return tokenId The ID of the token that represents the minted position
+    /// @return liquidity The amount of liquidity for this position
+    /// @return amount0 The amount of token0
+    /// @return amount1 The amount of token1
+    function mint(MintParams calldata params)
+        external
+        payable
+        returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1);
+}
