397f03b90d478dc01040c3afc274fcb11e09211c
Allen Lin <allenlin1992@hotmail.com>
2023-07-10 11:31:17 -0400

    add ERC721
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract NonfungiblePositionManager {}
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NonfungiblePositionManager is ERC721 {
+    constructor() ERC721("Uniswap V4 Positions NFT-V1", "UNI-V4-POS") {}
+}
