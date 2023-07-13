14b1c2ca4e66e93365bd46917f772da9676c353f
Allen Lin <allenlin1992@hotmail.com>
2023-07-13 13:13:12 -0400

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/NonfungiblePositionManager.t.sol
+++ test/NonfungiblePositionManager.t.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {TokenFixture} from "@uniswap/v4-core/test/foundry-tests/utils/TokenFixture.sol";
+import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
+import {NonfungiblePositionManager} from "../contracts/NonfungiblePositionManager.sol";
 
 contract NonfungiblePositionManagerTest is Test, TokenFixture {
+    PoolManager manager;
+    NonfungiblePositionManager nonfungiblePositionManager;
+
     function setUp() public {
         initializeTokens();
+        manager = new PoolManager(500000);
+        nonfungiblePositionManager = new NonfungiblePositionManager(manager, address(1));
     }
 }
