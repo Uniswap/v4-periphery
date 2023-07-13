f776f5a7eb9541c67ee1d670678e21db8a77481c
Allen Lin <allenlin1992@hotmail.com>
2023-07-13 15:41:32 -0400

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/NonfungiblePositionManager.t.sol
+++ test/NonfungiblePositionManager.t.sol
@@ -3,16 +3,31 @@ pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {TokenFixture} from "@uniswap/v4-core/test/foundry-tests/utils/TokenFixture.sol";
-import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
+import {PoolManager, IPoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
 import {NonfungiblePositionManager} from "../contracts/NonfungiblePositionManager.sol";
+import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
 
 contract NonfungiblePositionManagerTest is Test, TokenFixture {
     PoolManager manager;
     NonfungiblePositionManager nonfungiblePositionManager;
 
+    uint160 constant SQRT_RATIO_1_1 = 79228162514264337593543950336;
+
     function setUp() public {
         initializeTokens();
         manager = new PoolManager(500000);
         nonfungiblePositionManager = new NonfungiblePositionManager(manager, address(1));
+    }
+
+    function testMint() public {
+        IPoolManager.PoolKey memory key = IPoolManager.PoolKey({
+            currency0: currency0,
+            currency1: currency1,
+            fee: 3000,
+            hooks: IHooks(address(0)),
+            tickSpacing: 60
+        });
+
+        manager.initialize(key, SQRT_RATIO_1_1);
     }
 }
