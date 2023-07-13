cc0c6bc65ae7b00764d2c9b7d9ba9acdd064000d
Allen Lin <allenlin1992@hotmail.com>
2023-07-13 16:01:22 -0400

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/NonfungiblePositionManager.t.sol
+++ test/NonfungiblePositionManager.t.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.0;
 import {Test} from "forge-std/Test.sol";
 import {TokenFixture} from "@uniswap/v4-core/test/foundry-tests/utils/TokenFixture.sol";
 import {PoolManager, IPoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
-import {NonfungiblePositionManager} from "../contracts/NonfungiblePositionManager.sol";
+import {NonfungiblePositionManager, INonfungiblePositionManager} from "../contracts/NonfungiblePositionManager.sol";
 import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/libraries/PoolId.sol";
 
 contract NonfungiblePositionManagerTest is Test, TokenFixture {
+    using PoolIdLibrary for IPoolManager.PoolKey;
+
+    event ModifyPosition(
+        PoolId indexed poolId, address indexed sender, int24 tickLower, int24 tickUpper, int256 liquidityDelta
+    );
+
     PoolManager manager;
     NonfungiblePositionManager nonfungiblePositionManager;
 
     uint160 constant SQRT_RATIO_1_1 = 79228162514264337593543950336;
+    uint256 constant MAX_UINT256 = type(uint256).max;
 
     function setUp() public {
         initializeTokens();
@@ -29,5 +37,22 @@ contract NonfungiblePositionManagerTest is Test, TokenFixture {
         });
 
         manager.initialize(key, SQRT_RATIO_1_1);
+
+        vm.expectEmit(true, true, true, true);
+        emit ModifyPosition(key.toId(), address(nonfungiblePositionManager), 0, 60, 100);
+
+        nonfungiblePositionManager.mint(
+            INonfungiblePositionManager.MintParams({
+                poolKey: key,
+                tickLower: 0,
+                tickUpper: 60,
+                amount0Desired: 0,
+                amount1Desired: 0,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: address(this),
+                deadline: MAX_UINT256
+            })
+        );
     }
 }
