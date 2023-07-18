a204adefb70b8b230936351a687fc95d35729ffc
starit <janyucheng@gmail.com>
2023-07-18 17:13:43 +0800

    sync solc version with v4-core

--- contracts/hooks/examples/LimitOrder.sol
+++ contracts/hooks/examples/LimitOrder.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.19;
+pragma solidity =0.8.20;
 
 import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
 import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/libraries/PoolId.sol";
