8aae01b092076483169765ec28938fbaa9041558
Emily Williams <emag3m@gmail.com>
2023-09-01 16:57:11 -0400

    save DRY progress

--- contracts/Routing.sol
+++ contracts/Routing.sol
@@ -87,96 +87,66 @@ abstract contract Routing {
     }
 
     function _swapExactInputSingle(ExactInputSingleParams memory params, address msgSender) private {
-        BalanceDelta delta = poolManager.swap(
-            params.poolKey,
-            IPoolManager.SwapParams(
-                params.zeroForOne,
-                int256(int128(params.amountIn)),
-                params.sqrtPriceLimitX96 == 0
-                    ? (params.zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1)
-                    : params.sqrtPriceLimitX96
-            ),
-            bytes("")
+        _swapExactInputPrivate(
+            params.poolKey, params.zeroForOne, params.amountIn, params.sqrtPriceLimitX96, msgSender, true, true
         );
-
-        if (params.zeroForOne) {
-            _pay(
-                Currency.unwrap(params.poolKey.currency0),
-                msgSender,
-                address(poolManager),
-                uint256(uint128(delta.amount0()))
-            );
-            poolManager.settle(params.poolKey.currency0);
-            poolManager.take(params.poolKey.currency1, msgSender, uint256(uint128(-delta.amount1())));
-        } else {
-            _pay(
-                Currency.unwrap(params.poolKey.currency1),
-                msgSender,
-                address(poolManager),
-                uint256(uint128(delta.amount1()))
-            );
-            poolManager.settle(params.poolKey.currency1);
-            poolManager.take(params.poolKey.currency0, msgSender, uint256(uint128(-delta.amount0())));
-        }
     }
 
     function _swapExactInput(ExactInputParams memory params, address msgSender) private {
         for (uint256 i = 0; i < params.path.length; i++) {
             (PoolKey memory poolKey, bool zeroForOne) = _getPoolAndSwapDirection(params.path[i], params.currencyIn);
-            BalanceDelta delta = poolManager.swap(
+            uint128 amountOut = _swapExactInputPrivate(
                 poolKey,
-                IPoolManager.SwapParams(
-                    zeroForOne,
-                    int256(int128(params.amountIn)),
-                    params.sqrtPriceLimitX96 == 0
-                        ? (zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1)
-                        : params.sqrtPriceLimitX96
-                ),
-                bytes("")
+                zeroForOne,
+                params.amountIn,
+                params.sqrtPriceLimitX96,
+                msgSender,
+                i == 0,
+                i == params.path.length - 1
             );
 
-            if (i == 0) {
-                if (zeroForOne) {
-                    _pay(
-                        Currency.unwrap(poolKey.currency0),
-                        msgSender,
-                        address(poolManager),
-                        uint256(uint128(delta.amount0()))
-                    );
-                    poolManager.settle(poolKey.currency0);
-                } else {
-                    _pay(
-                        Currency.unwrap(poolKey.currency1),
-                        msgSender,
-                        address(poolManager),
-                        uint256(uint128(delta.amount1()))
-                    );
-                    poolManager.settle(poolKey.currency1);
-                }
-            }
-
-            if (i == params.path.length - 1) {
-                if (zeroForOne) {
-                    poolManager.take(poolKey.currency1, msgSender, uint256(uint128(-delta.amount1())));
-                } else {
-                    poolManager.take(poolKey.currency0, msgSender, uint256(uint128(-delta.amount0())));
-                }
-            }
-
-            if (zeroForOne) {
-                params.amountIn = uint128(-delta.amount1());
-            } else {
-                params.amountIn = uint128(-delta.amount0());
-            }
+            params.amountIn = amountOut;
             params.currencyIn = params.path[i].currencyOut;
         }
 
         if (params.amountIn < params.amountOutMinimum) revert TooLittleReceived();
     }
 
+    function _swapExactInputPrivate(
+        PoolKey memory poolKey,
+        bool zeroForOne,
+        uint128 amountIn,
+        uint160 sqrtPriceLimitX96,
+        address msgSender,
+        bool settle,
+        bool take
+    ) private returns (uint128 amountOut) {
+        BalanceDelta delta = poolManager.swap(
+            poolKey,
+            IPoolManager.SwapParams(
+                zeroForOne,
+                int256(int128(amountIn)),
+                sqrtPriceLimitX96 == 0
+                    ? (zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1)
+                    : sqrtPriceLimitX96
+            ),
+            bytes("")
+        );
+
+        if (zeroForOne) {
+            amountOut = uint128(-delta.amount1());
+            if (settle) _payAndSettle(poolKey.currency0, msgSender, delta.amount0());
+            if (take) poolManager.take(poolKey.currency1, msgSender, uint256(amountOut));
+        } else {
+            amountOut = uint128(-delta.amount0());
+            if (settle) _payAndSettle(poolKey.currency1, msgSender, delta.amount1());
+            if (take) poolManager.take(poolKey.currency0, msgSender, uint256(amountOut));
+        }
+    }
+
     function _getPoolAndSwapDirection(PathKey memory params, Currency currencyIn)
         private
-        view
+        pure
         returns (PoolKey memory poolKey, bool zeroForOne)
     {
         (Currency currency0, Currency currency1) =
@@ -184,6 +154,11 @@ abstract contract Routing {
 
         zeroForOne = currencyIn == currency0;
         poolKey = PoolKey(currency0, currency1, params.fee, params.tickSpacing, params.hooks);
+    }
+
+    function _payAndSettle(Currency currency, address msgSender, int128 settleAmount) private {
+        _pay(Currency.unwrap(currency), msgSender, address(poolManager), uint256(uint128(settleAmount)));
+        poolManager.settle(currency);
     }
 
     function _pay(address token, address payer, address recipient, uint256 amount) internal virtual;
