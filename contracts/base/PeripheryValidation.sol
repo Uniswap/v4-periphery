f9d830e7158970fb5cfb279b069224c846c09b86
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-11-22 12:57:02 -0500

    move custom errors inside contracts

--- contracts/base/PeripheryValidation.sol
+++ contracts/base/PeripheryValidation.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-error TransactionTooOld();
-
 abstract contract PeripheryValidation {
+    error TransactionTooOld();
+    
     modifier checkDeadline(uint256 deadline) {
         if (block.timestamp > deadline) revert TransactionTooOld();
         _;
