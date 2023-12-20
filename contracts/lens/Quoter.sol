f00e616555127d966e1d64521d6fae48b8943842
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2023-12-19 19:00:53 -0500

    remove console2 import

--- contracts/lens/Quoter.sol
+++ contracts/lens/Quoter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.20;
 
-import "forge-std/console2.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
