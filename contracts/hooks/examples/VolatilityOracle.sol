bd692c422a81c94deee3570bc4c6d45502f6ddfd
saucepoint <saucepoint@protonmail.com>
2024-03-14 12:41:13 +0000

    fix getSlot0 calls

--- contracts/hooks/examples/VolatilityOracle.sol
+++ contracts/hooks/examples/VolatilityOracle.sol
@@ -2,24 +2,23 @@
 pragma solidity ^0.8.19;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {IDynamicFeeManager} from "@uniswap/v4-core/src/interfaces/IDynamicFeeManager.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
-import {FeeLibrary} from "@uniswap/v4-core/src/libraries/FeeLibrary.sol";
+import {SwapFeeLibrary} from "@uniswap/v4-core/src/libraries/SwapFeeLibrary.sol";
 import {BaseHook} from "../../BaseHook.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 
-contract VolatilityOracle is BaseHook, IDynamicFeeManager {
-    using FeeLibrary for uint24;
+contract VolatilityOracle is BaseHook {
+    using SwapFeeLibrary for uint24;
 
     error MustUseDynamicFee();
 
     uint32 deployTimestamp;
 
-    function getFee(address, PoolKey calldata) external view returns (uint24) {
-        uint24 startingFee = 3000;
-        uint32 lapsed = _blockTimestamp() - deployTimestamp;
-        return startingFee + (uint24(lapsed) * 100) / 60; // 100 bps a minute
-    }
+    // function getFee(address, PoolKey calldata) external view returns (uint24) {
+    //     uint24 startingFee = 3000;
+    //     uint32 lapsed = _blockTimestamp() - deployTimestamp;
+    //     return startingFee + (uint24(lapsed) * 100) / 60; // 100 bps a minute
+    // }
 
     /// @dev For mocking
     function _blockTimestamp() internal view virtual returns (uint32) {
@@ -33,7 +32,7 @@ contract VolatilityOracle is BaseHook, IDynamicFeeManager {
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: true,
-            afterInitialize: false,
+            afterInitialize: true,
             beforeAddLiquidity: false,
             beforeRemoveLiquidity: false,
             afterAddLiquidity: false,
@@ -53,5 +52,16 @@ contract VolatilityOracle is BaseHook, IDynamicFeeManager {
     {
         if (!key.fee.isDynamicFee()) revert MustUseDynamicFee();
         return VolatilityOracle.beforeInitialize.selector;
+    }
+
+    function afterInitialize(address, PoolKey calldata key, uint160, int24, bytes calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        uint24 startingFee = 3000;
+        uint32 lapsed = _blockTimestamp() - deployTimestamp;
+        uint24 fee = startingFee + (uint24(lapsed) * 100) / 60; // 100 bps a minute
+        poolManager.updateDynamicSwapFee(key, fee); // initial fee 0.30%
     }
 }
