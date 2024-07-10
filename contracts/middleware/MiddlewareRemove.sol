fcd3b34e7cba934b5bb883e9879ae28d66e03176
Junion <69495294+Jun1on@users.noreply.github.com>
2024-07-10 18:03:52 -0400

    forbid after remove returns delta

--- contracts/middleware/MiddlewareRemove.sol
+++ contracts/middleware/MiddlewareRemove.sol
@@ -8,12 +8,24 @@ import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
 import {BaseMiddleware} from "./BaseMiddleware.sol";
 import {BaseHook} from "../BaseHook.sol";
 import {BalanceDeltaLibrary} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {CustomRevert} from "@uniswap/v4-core/src/libraries/CustomRevert.sol";
 
 contract MiddlewareRemove is BaseMiddleware {
+    using CustomRevert for bytes4;
+    using Hooks for IHooks;
+
+    error HookPermissionForbidden(address hooks);
+
     bytes internal constant ZERO_BYTES = bytes("");
     uint256 public constant gasLimit = 10000000;
 
-    constructor(IPoolManager _poolManager, address _impl) BaseMiddleware(_poolManager, _impl) {}
+    constructor(IPoolManager _poolManager, address _impl) BaseMiddleware(_poolManager, _impl) {
+        if (IHooks(address(this)).hasPermission(Hooks.AFTER_REMOVE_LIQUIDITY_RETURNS_DELTA_FLAG)) {
+            HookPermissionForbidden.selector.revertWith(address(this));
+        }
+    }
 
     function beforeRemoveLiquidity(
         address,
