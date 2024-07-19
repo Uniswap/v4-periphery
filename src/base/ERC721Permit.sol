558b6214c7ce5131c4bdd51172cc21205f545135
saucepoint <saucepoint@protonmail.com>
2024-07-19 17:25:56 -0400

    make DOMAIN_SEPARATOR immutable

--- src/base/ERC721Permit.sol
+++ src/base/ERC721Permit.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 import {ERC721} from "solmate/src/tokens/ERC721.sol";
 
-import {ChainId} from "../libraries/ChainId.sol";
 import {IERC721Permit} from "../interfaces/IERC721Permit.sol";
 
 /// @title ERC721 with permit
@@ -17,35 +16,35 @@ abstract contract ERC721Permit is ERC721, IERC721Permit {
     /// @dev The hash of the version string used in the permit signature verification
     bytes32 private immutable versionHash;
 
+    /// @inheritdoc IERC721Permit
+    bytes32 public immutable DOMAIN_SEPARATOR;
+
+    /// @inheritdoc IERC721Permit
+    /// @dev Value is equal to keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
+    bytes32 public constant override PERMIT_TYPEHASH =
+        0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
+
     /// @notice Computes the nameHash and versionHash
     constructor(string memory name_, string memory symbol_, string memory version_) ERC721(name_, symbol_) {
         nameHash = keccak256(bytes(name_));
         versionHash = keccak256(bytes(version_));
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
+                0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
+                nameHash,
+                versionHash,
+                block.chainid,
+                address(this)
+            )
+        );
     }
 
     // TODO: implement here, or in posm
     function tokenURI(uint256) public pure override returns (string memory) {
         return "https://example.com";
     }
-
-    /// @inheritdoc IERC721Permit
-    function DOMAIN_SEPARATOR() public view override returns (bytes32) {
-        return keccak256(
-            abi.encode(
-                // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
-                0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
-                nameHash,
-                versionHash,
-                ChainId.get(),
-                address(this)
-            )
-        );
-    }
-
-    /// @inheritdoc IERC721Permit
-    /// @dev Value is equal to keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
-    bytes32 public constant override PERMIT_TYPEHASH =
-        0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
 
     /// @inheritdoc IERC721Permit
     function permit(address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s)
@@ -94,9 +93,7 @@ abstract contract ERC721Permit is ERC721, IERC721Permit {
     {
         digest = keccak256(
             abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, _nonce, deadline))
+                "\x19\x01", DOMAIN_SEPARATOR, keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, _nonce, deadline))
             )
         );
     }
