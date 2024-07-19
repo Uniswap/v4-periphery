6c2147c3fbc10b686309ba0ca52c25ada452b871
saucepoint <saucepoint@protonmail.com>
2024-07-19 16:57:05 -0400

    test permit with multicall

--- test/position-managers/PositionManager.multicall.t.sol
+++ test/position-managers/PositionManager.multicall.t.sol
@@ -17,6 +17,7 @@ import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
 
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -35,11 +36,20 @@ contract MulticallTest is Test, Deployers, GasSnapshot, LiquidityFuzzers, Liquid
     using CurrencyLibrary for Currency;
     using LiquidityRangeIdLibrary for LiquidityRange;
     using Planner for Planner.Plan;
+    using PoolIdLibrary for PoolKey;
+    using StateLibrary for IPoolManager;
 
     PoolId poolId;
-    address alice = makeAddr("ALICE");
+    address alice;
+    uint256 alicePK;
+    address bob;
+
+    uint256 constant STARTING_USER_BALANCE = 10_000_000 ether;
 
     function setUp() public {
+        (alice, alicePK) = makeAddrAndKey("ALICE");
+        (bob,) = makeAddrAndKey("BOB");
+
         Deployers.deployFreshManagerAndRouters();
         Deployers.deployMintAndApprove2Currencies();
 
@@ -49,6 +59,20 @@ contract MulticallTest is Test, Deployers, GasSnapshot, LiquidityFuzzers, Liquid
 
         IERC20(Currency.unwrap(currency0)).approve(address(lpm), type(uint256).max);
         IERC20(Currency.unwrap(currency1)).approve(address(lpm), type(uint256).max);
+
+        // Give tokens to Alice and Bob, with approvals
+        IERC20(Currency.unwrap(currency0)).transfer(alice, STARTING_USER_BALANCE);
+        IERC20(Currency.unwrap(currency1)).transfer(alice, STARTING_USER_BALANCE);
+        IERC20(Currency.unwrap(currency0)).transfer(bob, STARTING_USER_BALANCE);
+        IERC20(Currency.unwrap(currency1)).transfer(bob, STARTING_USER_BALANCE);
+        vm.startPrank(alice);
+        IERC20(Currency.unwrap(currency0)).approve(address(lpm), type(uint256).max);
+        IERC20(Currency.unwrap(currency1)).approve(address(lpm), type(uint256).max);
+        vm.stopPrank();
+        vm.startPrank(bob);
+        IERC20(Currency.unwrap(currency0)).approve(address(lpm), type(uint256).max);
+        IERC20(Currency.unwrap(currency1)).approve(address(lpm), type(uint256).max);
+        vm.stopPrank();
     }
 
     function test_multicall_initializePool_mint() public {
@@ -79,5 +103,35 @@ contract MulticallTest is Test, Deployers, GasSnapshot, LiquidityFuzzers, Liquid
         swap(key, true, -1e18, ZERO_BYTES);
     }
 
-    function test_multicall_permitAndDecrease() public {}
+    function test_multicall_permitAndDecrease() public {
+        LiquidityRange memory range = LiquidityRange({poolKey: key, tickLower: -60, tickUpper: 60});
+        uint256 liquidityAlice = 1e18;
+        vm.startPrank(alice);
+        mint(range, liquidityAlice, alice, ZERO_BYTES);
+        vm.stopPrank();
+        uint256 tokenId = lpm.nextTokenId() - 1;
+
+        // Alice gives Bob permission to operate on her liquidity
+        uint256 nonce = 1;
+        bytes32 digest = lpm.getDigest(bob, tokenId, nonce, block.timestamp + 1);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePK, digest);
+
+        // bob gives himself permission and decreases liquidity
+        bytes[] memory calls = new bytes[](2);
+        calls[0] = abi.encodeWithSelector(
+            NonfungiblePositionManager(lpm).permit.selector, bob, tokenId, block.timestamp + 1, nonce, v, r, s
+        );
+        uint256 liquidityToRemove = 0.4444e18;
+        bytes memory actions = LiquidityOperations.getDecreaseEncoded(tokenId, 0.4444e18, ZERO_BYTES);
+        calls[1] =
+            abi.encodeWithSelector(NonfungiblePositionManager(lpm).modifyLiquidities.selector, actions, _deadline);
+
+        vm.prank(bob);
+        lpm.multicall(calls);
+
+        bytes32 positionId =
+            keccak256(abi.encodePacked(address(lpm), range.tickLower, range.tickUpper, bytes32(tokenId)));
+        (uint256 liquidity,,) = manager.getPositionInfo(range.poolKey.toId(), positionId);
+        assertEq(liquidity, liquidityAlice - liquidityToRemove);
+    }
 }
