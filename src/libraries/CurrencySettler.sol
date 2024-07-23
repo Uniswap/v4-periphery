9323625adaef50a9dbdb76e035646d4a320fda96
Alice Henshaw <henshawalice@gmail.com>
2024-07-23 13:52:15 +0100

    Safe transfer lib

--- src/libraries/CurrencySettler.sol
+++ src/libraries/CurrencySettler.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.24;
 
-import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
-import {IERC20Minimal} from "v4-core/interfaces/external/IERC20Minimal.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
 /// @notice Library used to interact with PoolManager.sol to settle any open deltas.
 /// To settle a positive delta (a credit to the user), a user may take or mint.
 /// To settle a negative delta (a debt on the user), a user make transfer or burn to pay off a debt.
 /// @dev Note that sync() is called before any erc-20 transfer in `settle`.
 library CurrencySettler {
+    using SafeTransferLib for ERC20;
+
     /// @notice Settle (pay) a currency to the PoolManager
     /// @param manager IPoolManager to settle to
     /// @param currency Currency to settle
@@ -21,9 +24,9 @@ library CurrencySettler {
         } else {
             manager.sync(currency);
             if (payer != address(this)) {
-                IERC20Minimal(Currency.unwrap(currency)).transferFrom(payer, address(manager), amount);
+                ERC20(Currency.unwrap(currency)).safeTransferFrom(payer, address(manager), amount);
             } else {
-                IERC20Minimal(Currency.unwrap(currency)).transfer(address(manager), amount);
+                ERC20(Currency.unwrap(currency)).safeTransfer(address(manager), amount);
             }
             manager.settle();
         }
