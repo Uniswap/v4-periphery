8cc9f90c019acbb686c1d91211824b34dae5d9d7
Alice Henshaw <henshawalice@gmail.com>
2024-07-24 18:25:06 +0100

    combine mocks

--- test/base/Multicall.t.sol
+++ test/base/Multicall.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import {MockMulticall} from "../mock/MockMulticall.sol";
+import {MockMulticall} from "../mocks/MockMulticall.sol";
 
 contract MulticallTest is Test {
     MockMulticall multicall;
