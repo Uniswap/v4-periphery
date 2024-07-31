0f03176a447a7a0c6d26bfee239226450f553c37
saucepoint <98790946+saucepoint@users.noreply.github.com>
2024-07-31 14:16:39 -0400

    test SafeCallback (#220)
    * test SafeCallback
    
    * assert PM address

---
+++ test/SafeCallback.t.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+
+import {SafeCallback} from "../src/base/SafeCallback.sol";
+import {MockSafeCallback} from "./mocks/MockSafeCallback.sol";
+
+contract SafeCallbackTest is Test, Deployers {
+    MockSafeCallback safeCallback;
+
+    function setUp() public {
+        deployFreshManager();
+        safeCallback = new MockSafeCallback(manager);
+    }
+
+    function test_poolManagerAddress() public view {
+        assertEq(address(safeCallback.poolManager()), address(manager));
+    }
+
+    function test_unlock(uint256 num) public {
+        bytes memory result = safeCallback.unlockManager(num);
+        assertEq(num, abi.decode(result, (uint256)));
+    }
+
+    function test_unlockRevert(address caller, bytes calldata data) public {
+        vm.startPrank(caller);
+        if (caller != address(manager)) vm.expectRevert(SafeCallback.NotPoolManager.selector);
+        safeCallback.unlockCallback(data);
+        vm.stopPrank();
+    }
+}
