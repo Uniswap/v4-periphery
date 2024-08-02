7e886da64e118bde6b4a2d4fa5e211244a3aafca
saucepoint <saucepoint@protonmail.com>
2024-08-02 16:58:43 -0400

    cleanup unused imports

--- src/base/Multicall.sol
+++ src/base/Multicall.sol
@@ -1,16 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.19;
 
-import {CustomRevert} from "@uniswap/v4-core/src/libraries/CustomRevert.sol";
-
 import {IMulticall} from "../interfaces/IMulticall.sol";
-
-import "forge-std/console2.sol";
 
 /// @title Multicall
 /// @notice Enables calling multiple methods in a single call to the contract
 abstract contract Multicall is IMulticall {
-    using CustomRevert for bytes4;
 
     /// @inheritdoc IMulticall
     function multicall(bytes[] calldata data) public payable override returns (bytes[] memory results) {
