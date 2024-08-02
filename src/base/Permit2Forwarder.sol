3442d3230bc0856d3b024fb9e1b0614861c10ebd
Sara Reynolds <snreynolds2506@gmail.com>
2024-08-02 12:15:19 -0400

    forwarder tests

--- src/base/Permit2Forwarder.sol
+++ src/base/Permit2Forwarder.sol
@@ -5,7 +5,6 @@ import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol"
 
 /// @notice PermitForwarder allows permitting this contract as a spender on permit2
 /// @dev This contract does not enforce the spender to be this contract, but that is the intended use case
-/// TODO: Does it need to be enforced?
 contract Permit2Forwarder {
     IAllowanceTransfer public immutable permit2;
 
@@ -14,15 +13,19 @@ contract Permit2Forwarder {
     }
 
     /// @notice allows forwarding a single permit to permit2
-    function selfPermit(address owner, IAllowanceTransfer.PermitSingle calldata permitSingle, bytes calldata signature)
+    /// @dev this function is payable to allow multicall with NATIVE bsed actions
+    function permit(address owner, IAllowanceTransfer.PermitSingle calldata permitSingle, bytes calldata signature)
         external
+        payable
     {
         permit2.permit(owner, permitSingle, signature);
     }
 
     /// @notice allows forwarding batch permits to permit2
-    function selfPermit(address owner, IAllowanceTransfer.PermitBatch calldata permitBatch, bytes calldata signature)
+    /// @dev this function is payable to allow multicall with NATIVE bsed actions
+    function permit(address owner, IAllowanceTransfer.PermitBatch calldata permitBatch, bytes calldata signature)
         external
+        payable
     {
         permit2.permit(owner, permitBatch, signature);
     }
