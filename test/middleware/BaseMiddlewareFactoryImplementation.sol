e52fb0477f0c2bf5d90711f96566546c562b97dc
Junion <69495294+Jun1on@users.noreply.github.com>
2024-08-02 15:51:24 -0400

    remove BaseMiddlewareFactory

--- test/middleware/BaseMiddlewareFactoryImplementation.sol
+++ test/middleware/BaseMiddlewareFactoryImplementation.sol
@@ -2,13 +2,30 @@
 pragma solidity ^0.8.24;
 
 import {BaseMiddlewareImplementation} from "./BaseMiddlewareImplemenation.sol";
-import {BaseMiddlewareFactory} from "./../../src/middleware/BaseMiddlewareFactory.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 
-contract BaseMiddlewareFactoryImplementation is BaseMiddlewareFactory {
-    constructor(IPoolManager _poolManager) BaseMiddlewareFactory(_poolManager) {}
+contract BaseMiddlewareFactoryImplementation {
+    event MiddlewareCreated(address implementation, address middleware);
 
-    function _deployMiddleware(address implementation, bytes32 salt) internal override returns (address middleware) {
+    mapping(address => address) private _implementations;
+
+    IPoolManager public immutable poolManager;
+
+    constructor(IPoolManager _poolManager) {
+        poolManager = _poolManager;
+    }
+
+    function getImplementation(address middleware) external view returns (address implementation) {
+        return _implementations[middleware];
+    }
+
+    function createMiddleware(address implementation, bytes32 salt) external returns (address middleware) {
+        middleware = _deployMiddleware(implementation, salt);
+        _implementations[middleware] = implementation;
+        emit MiddlewareCreated(implementation, middleware);
+    }
+
+    function _deployMiddleware(address implementation, bytes32 salt) internal returns (address middleware) {
         middleware = address(new BaseMiddlewareImplementation{salt: salt}(poolManager, implementation));
     }
 }
