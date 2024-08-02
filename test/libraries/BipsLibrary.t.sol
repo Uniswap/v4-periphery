ecf490a27afb2563c33451474f13fad897e88089
Alice Henshaw <henshawalice@gmail.com>
2024-08-02 17:08:02 +0100

    fuzz bips lib

---
+++ test/libraries/BipsLibrary.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import "forge-std/StdError.sol";
+import {BipsLibrary} from "../../src/libraries/BipsLibrary.sol";
+
+contract PositionConfigTest is Test {
+    using BipsLibrary for uint256;
+
+    function test_fuzz_calculatePortion(uint256 amount, uint256 bips) public {
+        amount = bound(amount, 0, uint256(type(uint128).max));
+        if (bips > 10000 || bips == 0) {
+            vm.expectRevert(BipsLibrary.InvalidBips.selector);
+            amount.calculatePortion(bips);
+        } else {
+            assertEq(amount.calculatePortion(bips), amount * bips / 10000);
+        }
+    }
+}
