57542bd6044bebcedb18a79608dba913d2029fc6
saucepoint <saucepoint@protonmail.com>
2024-08-02 19:48:33 -0400

    yall crazy for sending through the ringer

--- test/UnorderedNonce.t.sol
+++ test/UnorderedNonce.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import {UnorderedNonce} from "../src/base/UnorderedNonce.sol";
-import {UnorderedNonceLibrary} from "../src/libraries/UnorderedNonceLibrary.sol";
 import {MockUnorderedNonce} from "./mocks/MockUnorderedNonce.sol";
 
 contract UnorderedNonceTest is Test {
@@ -18,11 +17,11 @@ contract UnorderedNonceTest is Test {
         unorderedNonce.spendNonce(address(this), 0);
         unorderedNonce.spendNonce(address(this), 1);
 
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 1);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 5);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 0);
         unorderedNonce.spendNonce(address(this), 4);
     }
@@ -31,9 +30,9 @@ contract UnorderedNonceTest is Test {
         unorderedNonce.spendNonce(address(this), 255);
         unorderedNonce.spendNonce(address(this), 256);
 
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 255);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 256);
     }
 
@@ -41,9 +40,9 @@ contract UnorderedNonceTest is Test {
         unorderedNonce.spendNonce(address(this), 2 ** 240);
         unorderedNonce.spendNonce(address(this), 2 ** 240 + 1);
 
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 2 ** 240);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 2 ** 240 + 1);
 
         unorderedNonce.spendNonce(address(this), 2 ** 240 + 2);
@@ -52,13 +51,13 @@ contract UnorderedNonceTest is Test {
     function testInvalidateFullWord() public {
         unorderedNonce.batchSpendNonces(0, 2 ** 256 - 1);
 
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 0);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 1);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 254);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 255);
         unorderedNonce.spendNonce(address(this), 256);
     }
@@ -69,9 +68,9 @@ contract UnorderedNonceTest is Test {
         unorderedNonce.spendNonce(address(this), 0);
         unorderedNonce.spendNonce(address(this), 254);
         unorderedNonce.spendNonce(address(this), 255);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 256);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), 511);
         unorderedNonce.spendNonce(address(this), 512);
     }
@@ -88,21 +87,21 @@ contract UnorderedNonceTest is Test {
         nonce = bound(nonce, 0, (word + 2) * 256);
 
         if ((word * 256) <= nonce && nonce < ((word + 1) * 256)) {
-            vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+            vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         }
         unorderedNonce.spendNonce(address(this), nonce);
     }
 
     function test_fuzz_UsingNonceTwiceFails(uint256 nonce) public {
         unorderedNonce.spendNonce(address(this), nonce);
-        vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+        vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
         unorderedNonce.spendNonce(address(this), nonce);
     }
 
     function test_fuzz_UseTwoRandomNonces(uint256 first, uint256 second) public {
         unorderedNonce.spendNonce(address(this), first);
         if (first == second) {
-            vm.expectRevert(UnorderedNonceLibrary.NonceAlreadyUsed.selector);
+            vm.expectRevert(UnorderedNonce.NonceAlreadyUsed.selector);
             unorderedNonce.spendNonce(address(this), second);
         } else {
             unorderedNonce.spendNonce(address(this), second);
