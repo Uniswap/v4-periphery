c607129f5c37fdf1aaff014046f05a99afbc853a
Alice Henshaw <henshawalice@gmail.com>
2024-08-07 14:16:20 +0100

    Remove contract balance swap input

--- src/libraries/ActionConstants.sol
+++ src/libraries/ActionConstants.sol
@@ -5,6 +5,7 @@ library ActionConstants {
     /// @notice used to signal that an action should use the input value of the open delta on the pool manager
     /// or of the balance that the contract holds
     uint128 internal constant OPEN_DELTA = 0;
+    /// @notice used to signal that an action should use the contract's entire balance of a currency
     /// This value is equivalent to 1<<255, i.e. a singular 1 in the most significant bit.
     uint256 internal constant CONTRACT_BALANCE = 0x8000000000000000000000000000000000000000000000000000000000000000;
 
