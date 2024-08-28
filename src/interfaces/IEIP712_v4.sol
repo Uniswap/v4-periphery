b3ab9351f48b8212859f9bd9b66623a5765963ac
dianakocsis <diana.kocsis@uniswap.org>
2024-08-28 10:23:09 -0400

    fix natspec

--- src/interfaces/IEIP712_v4.sol
+++ src/interfaces/IEIP712_v4.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @notice This interface is used for an EIP712 implementation
 interface IEIP712_v4 {
-    /// @notice The EIP-712 domain separator for the current chain
+    /// @notice Returns the domain separator for the current chain.
     /// @return bytes32 The domain separator
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 }
