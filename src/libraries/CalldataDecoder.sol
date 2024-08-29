8f97c52943afad9e7dc4df0ae9acadd5094f677c
Alice Henshaw <henshawalice@gmail.com>
2024-08-29 17:47:51 +0100

    improved calldata checks

--- src/libraries/CalldataDecoder.sol
+++ src/libraries/CalldataDecoder.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import {PositionConfig} from "./PositionConfig.sol";
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {IV4Router} from "../interfaces/IV4Router.sol";
+import {console2} from "forge-std/console2.sol";
 
 /// @title Library for abi decoding in calldata
 library CalldataDecoder {
@@ -20,28 +21,8 @@ library CalldataDecoder {
         pure
         returns (bytes calldata actions, bytes[] calldata params)
     {
-        assembly ("memory-safe") {
-            // The offset of the 0th element is 0, which stores the offset of the length pointer of actions array.
-            // The offset of the 1st element is 32, which stores the offset of the length pointer of params array.
-            let actionsPtr := add(_bytes.offset, calldataload(_bytes.offset))
-            let paramsPtr := add(_bytes.offset, calldataload(add(_bytes.offset, 0x20)))
-
-            // The length is stored as the first element
-            actions.length := calldataload(actionsPtr)
-            params.length := calldataload(paramsPtr)
-
-            // The actual data is stored in the slot after the length
-            actions.offset := add(actionsPtr, 0x20)
-            params.offset := add(paramsPtr, 0x20)
-
-            // Calculate how far `params` is into the provided bytes
-            let relativeOffset := sub(params.offset, _bytes.offset)
-            // Check that that isnt longer than the bytes themselves, or revert
-            if lt(_bytes.length, add(params.length, relativeOffset)) {
-                mstore(0, SLICE_ERROR_SELECTOR)
-                revert(0, 0x04)
-            }
-        }
+        actions = _bytes.toBytes(0);
+        params = _bytes.toBytesArray(1);
     }
 
     /// @dev equivalent to: abi.decode(params, (uint256, PositionConfig, uint256, uint128, uint128, bytes)) in calldata
@@ -270,5 +251,35 @@ library CalldataDecoder {
             res.length := length
             res.offset := offset
         }
+    }
+
+    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes`
+    /// @param _bytes The input bytes string to extract a bytes string from
+    /// @param _arg The index of the argument to extract
+    function toBytesArray(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes[] calldata res) {
+        uint256 encodingLength;
+        assembly ("memory-safe") {
+            // The offset of the `_arg`-th element is `32 * arg`, which stores the offset of the length pointer.
+            // shl(5, x) is equivalent to mul(32, x)
+            let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, shl(5, _arg))))
+            // the number of byte strings in the byte array
+            let arrayLength := calldataload(lengthPtr)
+            // pointer to encoding of the final bytes string in the array is `32 * arrayLength`
+            let finalElementLengthPtr := add(add(lengthPtr, 0x20), calldataload(add(lengthPtr, shl(5, arrayLength))))
+            // the length of the final bytes string
+            let finalElementLength := calldataload(finalElementLengthPtr)
+            // the final bytes string's encoding is: 32 byte length, then the `length` bytes
+            let finalElementEnd := add(add(finalElementLengthPtr, finalElementLength), 0x20)
+            // total length needed in the calldata bytes array
+            encodingLength := sub(finalElementEnd, _bytes.offset)
+
+            // the start of the bytes string encoding is the slot after the length
+            let arrayOffset := add(lengthPtr, 0x20)
+            res.length := arrayLength
+            res.offset := arrayOffset
+        }
+
+        // check that the end of the variable length parameter is within the calldata provided
+        if (_bytes.length < encodingLength) revert SliceOutOfBounds();
     }
 }
