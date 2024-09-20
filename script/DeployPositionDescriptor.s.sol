69af3524ea3483bb34cb6598e538418c1dc73eb5
dianakocsis <diana.kocsis@uniswap.org>
2024-09-20 10:31:18 -0400

    deploy script

---
+++ script/DeployPositionDescriptor.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PositionDescriptor} from "../src/PositionDescriptor.sol";
+
+contract DeployPositionDescriptorTest is Script {
+    function setUp() public {}
+
+    function run(address poolManager, address weth, bytes32 nativeCurrencyLabel)
+        public
+        returns (PositionDescriptor positionDescriptor)
+    {
+        vm.startBroadcast();
+
+        positionDescriptor = new PositionDescriptor(
+            IPoolManager(poolManager),
+            weth,
+            nativeCurrencyLabel
+        );
+        console2.log("PositionDescriptor", address(positionDescriptor));
+
+        vm.stopBroadcast();
+    }
+}
