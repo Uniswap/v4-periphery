705a744b3412750b33df35e13c70544f4c40a765
saucepoint <saucepoint@protonmail.com>
2024-10-16 12:14:39 -0400

    natspec around inflated fee growth global

--- src/interfaces/ISubscriber.sol
+++ src/interfaces/ISubscriber.sol
@@ -5,18 +5,26 @@ import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
 
 /// @notice Interface that a Subscriber contract should implement to receive updates from the v4 position manager
 interface ISubscriber {
+    /// @notice Called when a position subscribes to this subscriber contract
     /// @param tokenId the token ID of the position
     /// @param data additional data passed in by the caller
     function notifySubscribe(uint256 tokenId, bytes memory data) external;
+    
     /// @notice Called when a position unsubscribes from the subscriber
     /// @dev This call's gas is capped at `unsubscribeGasLimit` (set at deployment)
     /// @dev Because of EIP-150, solidity may only allocate 63/64 of gasleft()
     /// @param tokenId the token ID of the position
     function notifyUnsubscribe(uint256 tokenId) external;
+    
+    /// @notice Called when a position modifies its liquidity or collects fees
     /// @param tokenId the token ID of the position
     /// @param liquidityChange the change in liquidity on the underlying position
     /// @param feesAccrued the fees to be collected from the position as a result of the modifyLiquidity call
+    /// @dev Note that feesAccrued can be artificially inflated by a malicious user
+    /// An actor can inflate feeGrowthGlobal (and consequently feesAccrued) by atomically donating and collecting the fees within the same unlockCallback
     function notifyModifyLiquidity(uint256 tokenId, int256 liquidityChange, BalanceDelta feesAccrued) external;
+    
+    /// @notice Called when a position transfers ownership
     /// @param tokenId the token ID of the position
     /// @param previousOwner address of the old owner
     /// @param newOwner address of the new owner
