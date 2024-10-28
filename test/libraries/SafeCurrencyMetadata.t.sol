ad52b77efd43132164bcb6bff696df26d012bf61
dianakocsis <diana.kocsis@uniswap.org>
2024-10-28 11:49:40 -0400

    format

--- test/libraries/SafeCurrencyMetadata.t.sol
+++ test/libraries/SafeCurrencyMetadata.t.sol
@@ -5,7 +5,6 @@ import "forge-std/Test.sol";
 import {SafeCurrencyMetadata} from "../../src/libraries/SafeCurrencyMetadata.sol";
 
 contract SafeCurrencyMetadataTest is Test {
-
     function test_truncateSymbol_succeeds() public pure {
         // 12 characters
         assertEq(SafeCurrencyMetadata.truncateSymbol("123456789012"), "123456789012");
@@ -14,5 +13,4 @@ contract SafeCurrencyMetadataTest is Test {
         // 14 characters
         assertEq(SafeCurrencyMetadata.truncateSymbol("12345678901234"), "123456789012");
     }
-
 }
