bf39426ed3013470fafbbe9ca1fca162d53d59c5
Sara Reynolds <30504811+snreynolds@users.noreply.github.com>
2024-11-23 08:28:33 +0000

    try-catch-initialize (#397)
    * try-catch-initialize
    
    * add test
    
    * fix return value
    
    * snapshot
    
    ---------
    
    Co-authored-by: Alice Henshaw <henshawalice@gmail.com>

--- src/base/PoolInitializer.sol
+++ src/base/PoolInitializer.sol
@@ -10,9 +10,15 @@ import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 /// @dev Enables create pool + mint liquidity in a single transaction with multicall
 abstract contract PoolInitializer is ImmutableState {
     /// @notice Initialize a Uniswap v4 Pool
+    /// @dev If the pool is already initialized, this function will not revert and just return type(int24).max
     /// @param key the PoolKey of the pool to initialize
     /// @param sqrtPriceX96 the initial sqrtPriceX96 of the pool
+    /// @return tick The current tick of the pool
     function initializePool(PoolKey calldata key, uint160 sqrtPriceX96) external payable returns (int24) {
-        return poolManager.initialize(key, sqrtPriceX96);
+        try poolManager.initialize(key, sqrtPriceX96) returns (int24 tick) {
+            return tick;
+        } catch {
+            return type(int24).max;
+        }
     }
 }
