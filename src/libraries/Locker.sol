d09f357d2bb77987547056b2715cdfc2d38b13a9
Sara Reynolds <snreynolds2506@gmail.com>
2024-11-25 12:49:41 -0500

    update license

--- src/libraries/Locker.sol
+++ src/libraries/Locker.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 /// @notice This is a temporary library that allows us to use transient storage (tstore/tload)
