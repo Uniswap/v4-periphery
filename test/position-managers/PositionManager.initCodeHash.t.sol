8bc2f5bb5057ee33e3fcae7ffa906d1c08e198a7
saucepoint <saucepoint@protonmail.com>
2024-12-12 12:04:12 -0500

    poc error

---
+++ test/position-managers/PositionManager.initCodeHash.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {PositionManager} from "../../src/PositionManager.sol";
+
+contract PositionManagerInitCodeHashTest is Test {
+    // function setUp() public {}
+    function test_initcodeHash() public {
+        vm.snapshotValue(
+            "positionManager initcode hash (without constructor params, as uint256)",
+            uint256(keccak256(type(PositionManager).creationCode))
+        );
+    }
+}
