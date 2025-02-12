c18b6a9d977ba1842090a12dda14f3e7d6b04a2f
saucepoint <saucepoint@protonmail.com>
2025-02-12 12:06:30 -0500

    test hooker miner against a basehook

--- test/libraries/HookMiner.t.sol
+++ test/libraries/HookMiner.t.sol
@@ -2,24 +2,22 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {HookMiner} from "../../src/libraries/HookMiner.sol";
-
-contract Blank {
-    uint256 public num;
-
-    constructor(uint256 _num) {
-        num = _num;
-    }
-}
+import {MockBlankHook} from "../mocks/MockBlankHook.sol";
 
 contract HookMinerTest is Test {
     function test_fuzz_hookMiner(uint16 flags, uint256 number) public {
-        (address addr, bytes32 salt) =
-            HookMiner.find(address(this), uint160(flags), type(Blank).creationCode, abi.encode(number));
+        (address addr, bytes32 salt) = HookMiner.find(
+            address(this),
+            uint160(flags),
+            type(MockBlankHook).creationCode,
+            abi.encode(IPoolManager(address(0)), number, flags)
+        );
 
-        Blank c = new Blank{salt: salt}(number);
-
+        MockBlankHook c = new MockBlankHook{salt: salt}(IPoolManager(address(0)), number, flags);
+        c.forceValidateAddress();
         assertEq(address(c), addr);
         assertEq(c.num(), number);
 
@@ -32,8 +30,9 @@ contract HookMinerTest is Test {
         uint16 flags = uint16(Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG);
         uint256 number = 100;
         (address addr, bytes32 salt) =
-            HookMiner.find(address(this), uint160(flags), type(Blank).creationCode, abi.encode(number));
-        Blank c = new Blank{salt: salt}(number);
+            HookMiner.find(address(this), uint160(flags), type(MockBlankHook).creationCode, abi.encode(IPoolManager(address(0)), number, flags));
+        MockBlankHook c = new MockBlankHook{salt: salt}(IPoolManager(address(0)), number, flags);
+        c.forceValidateAddress();
         assertEq(address(c), addr);
         assertEq(c.num(), number);
 
@@ -42,14 +41,15 @@ contract HookMinerTest is Test {
 
         // despite using the same `.find()` parameters, the library skips any addresses with bytecode
         (address newAddress, bytes32 otherSalt) =
-            HookMiner.find(address(this), uint160(flags), type(Blank).creationCode, abi.encode(number));
+            HookMiner.find(address(this), uint160(flags), type(MockBlankHook).creationCode, abi.encode(IPoolManager(address(0)), number, flags));
 
         // different salt / address was found
         assertNotEq(newAddress, addr);
         assertNotEq(otherSalt, salt);
 
         // second contract deploys successfully with the unique salt
-        Blank d = new Blank{salt: otherSalt}(number);
+        MockBlankHook d = new MockBlankHook{salt: otherSalt}(IPoolManager(address(0)), number, flags);
+        d.forceValidateAddress();
         assertEq(address(d), newAddress);
         assertEq(d.num(), number);
 
