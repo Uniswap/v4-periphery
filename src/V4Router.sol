7193dd5fa45436032b14c1126f08da2644172558
luke miles <luke@context.wtf>
2025-02-12 11:48:15 -0500

    misc: update solc to 0.8.28

--- src/V4Router.sol
+++ src/V4Router.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity 0.8.28;
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
