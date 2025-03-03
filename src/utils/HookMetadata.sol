a27cb9c588c7817aa331b9ee60278df193acbe00
smak0v <makov.sergey.it@gmail.com>
2025-03-03 18:05:55 +0000

    HookMetadata moved to utils

--- src/utils/HookMetadata.sol
+++ src/utils/HookMetadata.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IHookMetadata} from "../../interfaces/IHookMetadata.sol";
+import {IHookMetadata} from "../interfaces/IHookMetadata.sol";
 
 /**
  * @title HookMetadata
