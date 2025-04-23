15689ae1e553c4b45ece2089ac401acf0f3fa434
Mark Toda <toda.mark@gmail.com>
2025-04-23 13:10:00 -0400

    fix: carat for base files

--- src/utils/BaseHook.sol
+++ src/utils/BaseHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
 
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
