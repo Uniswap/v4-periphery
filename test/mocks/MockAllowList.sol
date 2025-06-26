be734cf96a23c3ca36e62e3cdf36ffcd2a2ab70c
ericneil-sanc <eric.sanchirico@uniswap.org>
2025-06-26 09:59:24 -0400

    use BaseAllowList not BaseAllowlist

--- test/mocks/MockAllowList.sol
+++ test/mocks/MockAllowList.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {IAllowlistChecker} from "../../src/hooks/permissionedPools/interfaces/IAllowlistChecker.sol";
-import {BaseAllowlistChecker} from "../../src/hooks/permissionedPools/BaseAllowlistChecker.sol";
+import {BaseAllowlistChecker} from "../../src/hooks/permissionedPools/BaseAllowListChecker.sol";
 
 contract MockAllowList is BaseAllowlistChecker {
     // Mapping to track allowed addresses
